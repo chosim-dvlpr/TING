37076b9843932360216e480e81eae4c2d2f63a36
hayeongK <haha990691@gmail.com>
2023-07-26 22:57:20 +0900

    feat: get qna

--- ting-backend/src/main/java/com/ssafy/tingbackend/mypage/repository/QnARepository.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/mypage/repository/QnARepository.java
@@ -1,8 +1,13 @@
 package com.ssafy.tingbackend.mypage.repository;
 
 import com.ssafy.tingbackend.entity.QnA;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QnARepository extends JpaRepository<QnA, Long> {
+import java.util.List;
 
+public interface QnARepository extends JpaRepository<QnA, Long> {
+    List<QnA> findAllByUserId(Long userId);
+    Page<QnA> findByUserId(Long userId, Pageable pageable);
 }
