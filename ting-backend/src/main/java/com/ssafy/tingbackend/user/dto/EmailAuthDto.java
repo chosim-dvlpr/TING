6360eb68a84f1e0fb4354fd7330b90b0c0115d11
cksghks89 <song960705@naver.com>
2023-07-26 14:05:04 +0900

    chore: mongodb 설정 추가

--- ting-backend/src/main/java/com/ssafy/tingbackend/user/dto/EmailAuthDto.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/user/dto/EmailAuthDto.java
@@ -1,18 +1,13 @@
 package com.ssafy.tingbackend.user.dto;
 
-import com.mongodb.DBObject;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.bson.BSONObject;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Map;
-import java.util.Set;
 
 @Document(collection = "email")
 @Getter
@@ -25,7 +20,7 @@ public class EmailAuthDto {
     private String email;
     private String key;
 
-    @Indexed(expireAfterSeconds = 5)
+    @Indexed(expireAfterSeconds = 600)
     private LocalDateTime createdAt;
 
     public EmailAuthDto(String email, String key) {
