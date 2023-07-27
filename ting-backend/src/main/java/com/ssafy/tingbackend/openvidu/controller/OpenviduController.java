b7e36d8fdc52c4865d46f90850e7940b35dd452f
cksghks89 <song960705@naver.com>
2023-07-27 12:21:03 +0900

    chore: cross origin 설정 변경

--- ting-backend/src/main/java/com/ssafy/tingbackend/openvidu/controller/OpenviduController.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/openvidu/controller/OpenviduController.java
@@ -6,10 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.Map;
@@ -17,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class OpenviduController {
 
     @Value("${openvidu.url}")
