1b220891db9eb6355ea50a107c0f46d83b9aba8c
sujeong <worend365@naver.com>
2023-07-27 19:51:02 +0900

    feat: MatchingRepository 추가

---
+++ ting-backend/src/main/java/com/ssafy/tingbackend/matching/repository/MatchingRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.tingbackend.matching.repository;
+
+import com.ssafy.tingbackend.entity.matching.Matching;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchingRepository extends JpaRepository<Matching, Long> {
+
+}
