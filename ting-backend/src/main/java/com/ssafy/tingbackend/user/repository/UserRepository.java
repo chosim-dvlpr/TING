4b1b0ed5f8510469f20f3e57aa7a898fbd83ce7f
cksghks89 <song960705@naver.com>
2023-07-29 17:13:48 +0900

    feat: 관리자 회원삭제 API 구현

--- ting-backend/src/main/java/com/ssafy/tingbackend/user/repository/UserRepository.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.ssafy.tingbackend.user.repository;
 
 import com.ssafy.tingbackend.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -21,4 +23,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.name=:name AND u.phoneNumber=:phoneNumber AND u.email=:email AND u.isRemoved=false")
     Optional<User> findPassword(String name, String phoneNumber, String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.isRemoved = true, u.removedTime = :removedTime WHERE u.id = :userId")
+    void softDeleteUser(@Param("userId") Long userId, @Param("removedTime") LocalDateTime removedTime);
 }
