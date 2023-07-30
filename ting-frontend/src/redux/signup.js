39bb6c3283fc371b7ceac2eea4e704ecacae9188
bmj0053 <bmj05378@gmail.com>
2023-07-30 18:51:48 +0900

    feat: signup reducer 선택항목 함수 추가

--- ting-frontend/src/redux/signup.js
+++ ting-frontend/src/redux/signup.js
@@ -1,90 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// const initState = {
-//   email: "",
-//   password: "",
-//   phonenumber: "",
-//   name: "",
-//   nickname: "",
-//   gender: "",
-//   birth: "",
-//   region: "",
-// }
-
-// const TEXT = "text";
-// const SET_EMAIL = "SET_EMAIL";
-// const SET_PASSWORD = "SET_PASSWORD";
-// const SET_PHONENUMBER = "SET_PHONENUMBER";
-// const SET_NAME = "SET_NAME";
-// const SET_NICKNAME = "SET_NICKNAME";
-// const SET_GENDER = "SET_GENDER";
-// const SET_BIRTH = "SET_BIRTH";
-// const SET_REGION = "SET_REGION";
-
-// const signupReducer = (state = initState, action) => {
-//   switch (action.type) {
-//     case TEXT:
-//       return {
-//         ...state, // 불변성 유지
-//         // type: "text",
-//         email: action.email, // 변경할 state 값
-//         password: action.password,
-//         phonenumber: action.phonenumber,
-//         name: action.name,
-//         nickname: action.nickname,
-//         gender: action.gender,
-//         birth: action.birth,
-//         region: action.region,
-//       };
-//     case SET_EMAIL:
-//       return {
-//         ...state,
-//         email: action.email,
-//       };
-//     case SET_PASSWORD:
-//       return {
-//         ...state,
-//         password: action.password,
-//       };
-//     case SET_PHONENUMBER:
-//       return {
-//         ...state,
-//         phonenumber: action.phonenumber,
-//       };
-//     case SET_NAME:
-//       return {
-//         ...state,
-//         name: action.name,
-//       };
-//     case SET_NICKNAME:
-//       return {
-//         ...state,
-//         nickname: action.nickname,
-//       };
-//     case SET_GENDER:
-//       return {
-//         ...state,
-//         gender: action.gender,
-//       };
-//     case SET_BIRTH:
-//       return {
-//         ...state,
-//         birth: action.birth,
-//       };
-//     case SET_REGION:
-//       return {
-//         ...state,
-//         region: action.region,
-//       };
-//     default:
-//       return state;
-//     }
-//   // 뒤로가기 눌렀을 때 데이터를 초기화할지?
-// };
-
 export const signupReducer = createSlice({
   name: 'signupReducer',
   initialState: {
+    // 기본값
     email: "",
     password: "",
     phonenumber: "",
@@ -93,6 +12,18 @@ export const signupReducer = createSlice({
     gender: "",
     birth: "",
     region: "",
+    // 선택값
+    profileImage: "",
+    mbtiCode: "",
+    heightCode: "",
+    drinkingCode: "",
+    smokingCode: "",
+    religionCode: "",
+    hobbyCodeList: [],
+    jobCode: "",
+    personalityCodeList: [],
+    introduction: "",
+    styleCodeList: [],
   },
   reducers: {
     setEmail: (state, action) => {
@@ -119,47 +50,53 @@ export const signupReducer = createSlice({
     setBirth: (state, action) => {
       state.birth = action.payload;
     },
-    // setName: (state, action) => {
-    //   state.name = action.payload;
-    // },
-    // setName: (state, action) => {
-    //   state.name = action.payload;
-    // },
-    // setName: (state, action) => {
-    //   state.name = action.payload;
-    // },
-    // setName: (state, action) => {
-    //   state.name = action.payload;
-    // },
-    // setName: (state, action) => {
-    //   state.name = action.payload;
-    // },
+    // 여기부터는 선택값
+    setMbtiCode: (state, action) => {
+      state.mbtiCode = action.payload;
+    },
+    setHeightCode: (state, action) => {
+      state.heightCode = action.payload;
+    },
+    setDrinkingCode: (state, action) => {
+      state.drinkingCode = action.payload;
+    },
+    setSmokingCode: (state, action) => {
+      state.smokingCode = action.payload;
+    },
+    setReligionCode: (state, action) => {
+      state.religionCode = action.payload;
+    },
+    setAddHobbyCodeList: (state, action) => {
+      state.hobbyCodeList.push(action.payload);
+    },
+    setDeleteHobbyCodeList: (state, action) => {
+      state.hobbyCodeList.slice(action.payload, 1);
+    },
+    setJobCode: (state, action) => {
+      state.jobCode = action.payload;
+    },
+    setAddPersonalityCodeList: (state, action) => {
+      state.personalityCodeList.push(action.payload);
+    },
+    setDeletePersonalityCodeList: (state, action) => {
+      state.personalityCodeList.slice(action.payload, 1);
+    },
+    setIntroduction: (state, action) => {
+      state.introduction = action.payload;
+    },
+    setAddStyleCodeList: (state, action) => {
+      state.styleCodeList.push(action.payload);
+    },
+    setDeleteStyleCodeList: (state, action) => {
+      state.styleCodeList.slice(action.payload, 1);
+    },
   }
 })
 
 
-// const signupReducer = (state = initState, action) => {
-//   switch (action.type) {
-//     // 성공 시
-//     case 200:
-//       return {
-//         ...state, // 불변성 유지
-//         email: action.email, // 변경할 state 값
-//         password: action.password,
-//         phonenumber: action.phonenumber,
-//         name: action.name,
-//         nickname: action.nickname,
-//         gender: action.gender,
-//         birth: action.birth,
-//         region: action.region,
-//       };
-//     // 기본 값
-//     default:
-//       return state;
-//     // 뒤로가기 눌렀을 때 데이터를 초기화할지?
-//   }
-// };
-
-// export default signupReducer;
-export const { setEmail, setPassword, setPhonenumber, setName, setNickname, setGender, setRegion, setBirth } = signupReducer.actions;
+export const { setEmail, setPassword, setPhonenumber, setName, setNickname, setGender, setRegion, setBirth,
+  setMbtiCode, setHeightCode, setDrinkingCode, setSmokingCode, setReligionCode, setAddHobbyCodeList,
+  setDeleteHobbyCodeList, setJobCode, setAddPersonalityCodeList, setDeletePersonalityCodeList, setIntroduction,
+  setAddStyleCodeList, setDeleteStyleCodeList, 
+} = signupReducer.actions;
 export default signupReducer;
