aab563a7eedba5d5e6fc510c361dc52fdec35d99
hayeongK <haha990691@gmail.com>
2023-07-31 21:43:11 +0900

    feat: get, alive, delete friend

--- ting-backend/src/main/java/com/ssafy/tingbackend/friend/controller/FriendController.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/friend/controller/FriendController.java
@@ -1,14 +1,14 @@
 package com.ssafy.tingbackend.friend.controller;
 
 import com.ssafy.tingbackend.board.dto.AdviceBoardDto;
+import com.ssafy.tingbackend.common.response.CommonResponse;
 import com.ssafy.tingbackend.common.response.DataResponse;
 import com.ssafy.tingbackend.friend.dto.ChattingDto;
 import com.ssafy.tingbackend.friend.service.FriendService;
+import com.ssafy.tingbackend.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -29,5 +29,38 @@ public class FriendController {
         Long userId = Long.parseLong(principal.getName());
         List<ChattingDto> chattingList = friendService.chattingList(userId);
         return new DataResponse<>(200, "친구 목록 조회 성공", chattingList);
+    }
+
+    /**
+     * 친구 프로필 조회 API
+     * @param userId
+     * @return 친구 프로필 정보
+     */
+    @GetMapping("/friend/profile/{userId}")
+    public DataResponse<UserDto.Info> friendInfo(@PathVariable Long userId) {
+        UserDto.Info userDto = friendService.friendInfo(userId);
+        return new DataResponse<>(200, "친구 프로필 조회 성공", userDto);
+    }
+
+    /**
+     * 친구 부활 API
+     * @param chattingId
+     * @return Only code and message
+     */
+    @PutMapping("/friend/{chattingId}")
+    public CommonResponse aliveFriend(@PathVariable Long chattingId) {
+        friendService.aliveFriend(chattingId);
+        return new CommonResponse(200, "친구 부활 성공");
+    }
+
+    /**
+     * 친구 삭제 API
+     * @param chattingId
+     * @return Only code and message
+     */
+    @DeleteMapping("/friend/{chattingId}")
+    public CommonResponse deleteFriend(@PathVariable Long chattingId) {
+        friendService.deleteFriend(chattingId);
+        return new CommonResponse(200, "친구 삭제 성공");
     }
 }
