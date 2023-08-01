09f2a74ada749ccecfa9a2ffdcbe38c83b2dd561
정수정 <worend365@naver.com>
2023-08-01 14:28:07 +0900

    feat: 매칭 웹소켓 연결 방식 변경

---
+++ ting-backend/src/main/java/com/ssafy/tingbackend/chatting/service/TemperatureService.java
@@ -0,0 +1,12 @@
+package com.ssafy.tingbackend.chatting.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TemperatureService {
+
+}
