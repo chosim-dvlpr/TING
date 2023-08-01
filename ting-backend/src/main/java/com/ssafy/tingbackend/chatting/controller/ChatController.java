b191b89dcdb3ff90e80f355b7f176474238bb02a
김하영 <haha0691@koreatech.ac.kr>
2023-08-01 17:47:04 +0900

    feat: chat

---
+++ ting-backend/src/main/java/com/ssafy/tingbackend/chatting/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.ssafy.tingbackend.chatting.controller;
+
+import com.ssafy.tingbackend.chatting.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+}
