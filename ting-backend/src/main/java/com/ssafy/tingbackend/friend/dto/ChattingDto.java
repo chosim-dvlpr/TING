2ecac4b6d78b53e4a5066aa1448af2832309032a
김하영 <haha0691@koreatech.ac.kr>
2023-08-03 14:59:17 +0900

    feat: last message

--- ting-backend/src/main/java/com/ssafy/tingbackend/friend/dto/ChattingDto.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/friend/dto/ChattingDto.java
@@ -6,6 +6,7 @@ import com.ssafy.tingbackend.entity.user.User;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -15,6 +16,7 @@ import java.math.BigDecimal;
 public class ChattingDto {
     private Long chattingId;
     private String lastChattingContent;
+    private LocalDateTime lastChattingTime;
     private ChattingType state;
     private BigDecimal temperature;
     private String profileImage;
@@ -26,6 +28,7 @@ public class ChattingDto {
         return ChattingDto.builder()
                 .chattingId(chatting.getId())
                 .lastChattingContent(chatting.getLastChattingContent())
+                .lastChattingTime(chatting.getLastChattingTime())
                 .state(chatting.getState())
                 .temperature(chatting.getTemperature())
                 .profileImage(friend.getProfileImage())
