4db03863186ce55b6db4c07891368389e620c7ca
chosim-dvlpr <bmj05378@gmail.com>
2023-08-04 17:08:50 +0900

    feat: 채팅 db 연결

--- ting-frontend/src/component/friend/FriendChatting.js
+++ ting-frontend/src/component/friend/FriendChatting.js
@@ -2,12 +2,37 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import useMessageStore from './useMessageStore';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import tokenHttp from '../../api/tokenHttp';
 
 function FriendChatting() {
   const location = useLocation();
   const Navigate = useNavigate();
   const messageStore = useMessageStore();
-  let userdata = useSelector((state) => state.userdataReducer);
+  // let userdata = useSelector((state) => state.userdataReducer);
+
+  const [previousMessage, setPreviousMessage] = useState([]);
+
+  // 처음 렌더링 시 기존 데이터 DB에서 받아오기
+  const getPreviousMessage = () => {
+    tokenHttp.get(`/chatting/${location.state.friend.chattingId}`).then((response) => {
+      // 불러오기 성공 시 previousMessage에 대화 내용 저장
+      if (response.data.code === 200) {
+        console.log('로그 불러오기 성공');
+        setPreviousMessage([...response.data.data.chattingList]); // 대화 내용 저장
+      }
+      else if (response.data.code === 400) {
+        console.log('실패');
+      }
+    })
+    .catch(() => console.log("실패"));
+  }
+
+  // 처음 렌더링 시 실행
+  useEffect(() => {
+    getPreviousMessage(); // 불러온 데이터를 messageLogs에 저장
+  }, [])
+
 
   const {
     connected,
