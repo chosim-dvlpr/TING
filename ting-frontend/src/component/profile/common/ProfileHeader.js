62048c129b87d9369c4df4d1efc92faa90eb0058
bmj0053 <bmj05378@gmail.com>
2023-08-06 01:15:44 +0900

    feat: 프로필 헤더 구현

--- ting-frontend/src/component/profile/common/ProfileHeader.js
+++ ting-frontend/src/component/profile/common/ProfileHeader.js
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
+
 function ProfileHeader() {
-    return (
-        <div>프로필 헤더</div>
-    )
+  let userdata = useSelector((state) => state.userdataReducer.userdata);
+
+  return (
+    <div>
+      <h1>프로필 헤더</h1>
+      
+      <p>프로필 이미지</p>
+
+      <p>이름 : { userdata.name }</p>
+      <p>자기소개 : { userdata.introduce }</p>
+    </div>
+  )
 }
 
 export default ProfileHeader
