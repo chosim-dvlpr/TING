0f3f4e9683322cc5f0f857502ce344b617e1901f
이 은 석 <modelism93@naver.com>
2023-08-05 13:17:45 +0900

    feat : 끝 카드 5초로 바꾸기

--- ting-frontend/src/component/matching/asset/TimerBar.js
+++ ting-frontend/src/component/matching/asset/TimerBar.js
@@ -34,25 +34,28 @@ function TimerBar(props) {
       console.log("타이머끝낫어!");
       setCount(30)
       // 타임이 끝나면 5점을 자동으로 상대에게 전달
-      // openviduSession.signal({
-      //   data: JSON.stringify({ score: 5 }),
-      //   to: [],
-      //   type: "score",
-      // });
+      openviduSession.signal({
+        data: JSON.stringify({ score: 5 }),
+        to: [],
+        type: "score",
+      });
 
       // TODO: myScore에 5점 추가하는 로직
       dispatch(setMyScore(5))
-      console.log('이거 세번 호출?')
       dispatch(setQuestionNumber(questionNumber+1))
       
       // TODO: API로 점수 저장하는 로직
-
     }
     return () => {
       clearInterval(timer);
     };
   }, [count]);
 
+  useEffect(()=>{
+    if (questionNumber === 11){
+      setCount(5)
+    }
+  }, [questionNumber])
 
   return <h3>{count}</h3>;
 
