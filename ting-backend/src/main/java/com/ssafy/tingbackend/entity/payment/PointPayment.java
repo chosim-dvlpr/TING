ca753ef1a613cedaf9425add5c73c0e0a20e59a7
cksghks89 <song960705@naver.com>
2023-08-07 10:46:43 +0900

    feat: kakaopay 관련 엔티티 생성

---
+++ ting-backend/src/main/java/com/ssafy/tingbackend/entity/payment/PointPayment.java
@@ -0,0 +1,38 @@
+package com.ssafy.tingbackend.entity.payment;
+
+import com.ssafy.tingbackend.entity.type.PaymentMethodType;
+import com.ssafy.tingbackend.entity.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointPayment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "point_code")
+    private PointCode pointCode;
+
+    private int quantity;
+    private String cid;
+    private String aid;
+    private String tid;
+    private String pgToken;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethodType paymentMethodType;
+
+}
