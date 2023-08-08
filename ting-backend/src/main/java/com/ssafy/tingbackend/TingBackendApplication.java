3e6cdc6216e64244df20c0f4257754ac826c4cc7
cksghks89 <song960705@naver.com>
2023-08-09 08:48:33 +0900

    chore: 스프링 Timezone ì한국시간으로 변경

--- ting-backend/src/main/java/com/ssafy/tingbackend/TingBackendApplication.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/TingBackendApplication.java
@@ -1,15 +1,23 @@
 package com.ssafy.tingbackend;
 
-import com.ssafy.tingbackend.friend.service.TemperatureService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class TingBackendApplication {
+
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TingBackendApplication.class, args);
