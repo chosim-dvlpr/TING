2d2dee6f155103b3687fd8d84f32617fa5af9ac0
ChanHwan <song960705@naver.com>
2023-08-10 01:44:04 +0900

    feat: dashboard 데이터 바인딩 ;

--- ting-frontend/src/component/admin/Dashboard.js
+++ ting-frontend/src/component/admin/Dashboard.js
@@ -1,8 +1,45 @@
 import styles from "./Dashboard.module.css";
 import { PriceChart } from "./PriceChart";
 import { MatchingChart } from "./MatchingChart";
+import { useEffect, useState } from "react";
+import tokenHttp from "../../api/tokenHttp";
 
 const Dashboard = () => {
+  const [matchingCount, setMatchingCount] = useState(0);
+  const [userCount, setUserCount] = useState(0);
+  const [reportCount, setReportCount] = useState(0);
+  const [totalProfit, setTotalProfit] = useState(0);
+
+  useEffect(() => {
+    tokenHttp
+      .get("/admin/matching/count")
+      .then((response) => {
+        setMatchingCount(response.data.data["totalCount"]);
+      })
+      .catch((error) => {});
+
+    tokenHttp
+      .get("/admin/user/count")
+      .then((response) => {
+        setUserCount(response.data.data["totalCount"]);
+      })
+      .catch((error) => {});
+
+    tokenHttp
+      .get("/admin/report/count")
+      .then((response) => {
+        setReportCount(response.data.data["totalCount"]);
+      })
+      .catch((error) => {});
+
+    tokenHttp
+      .get("/admin/profit/total")
+      .then((response) => {
+        setTotalProfit(response.data.data["totalProfit"]);
+      })
+      .catch((error) => {});
+  }, []);
+
   return (
     <div>
       <div className={styles.title}>Dashboard</div>
@@ -16,7 +53,7 @@ const Dashboard = () => {
             <div className={styles.summaryText}>
               <div className={styles.text}>총 매칭 횟수</div>
               <div className={styles.info}>
-                <span>4,819</span> 회
+                <span>{matchingCount.toLocaleString()}</span> 회
               </div>
             </div>
             <hr className={styles.summaryHr} />
@@ -30,7 +67,7 @@ const Dashboard = () => {
             <div className={styles.summaryText}>
               <div className={styles.text}>총 회원수</div>
               <div className={styles.info}>
-                <span>2,891</span> 명
+                <span>{userCount.toLocaleString()}</span> 명
               </div>
             </div>
             <hr className={styles.summaryHr} />
@@ -44,7 +81,7 @@ const Dashboard = () => {
             <div className={styles.summaryText}>
               <div className={styles.text}>미확인 문의 내역</div>
               <div className={styles.info}>
-                <span>27</span>
+                <span>{reportCount}</span>
               </div>
             </div>
             <hr className={styles.summaryHr} />
@@ -58,7 +95,7 @@ const Dashboard = () => {
             <div className={styles.summaryText}>
               <div className={styles.text}>총 매출액</div>
               <div className={styles.info}>
-                &#8361;<span>203,102,000</span>
+                &#8361;<span>{totalProfit.toLocaleString()}</span>
               </div>
             </div>
             <hr className={styles.summaryHr} />
