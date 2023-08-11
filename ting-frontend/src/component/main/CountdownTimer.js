491310af4edfcdb273dfeb81baaf97a7740c72d4
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-08-11 22:40:07 +0900

    fix:메인 페이지 밀리초 부분 수정

--- ting-frontend/src/component/main/CountdownTimer.js
+++ ting-frontend/src/component/main/CountdownTimer.js
@@ -1,33 +1,36 @@
-// CountdownTimer.js
-
 import React, { useState, useEffect } from 'react';
 import styles from './CountdownTimer.module.css';
 
 const CountdownTimer = () => {
-    const [remainingTime, setRemainingTime] = useState(300); // 5 minutes in seconds
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        if (remainingTime > 0) {
-          setRemainingTime(prevTime => prevTime - 0.001); // 1 밀리초 감소
-        }
-      }, 1); // 1밀리초 간격으로 실행
-  
-      return () => clearInterval(interval);
-    }, [remainingTime]);
-  
-    const formatTime = time => {
-      const minutes = Math.floor(time / 60);
-      const seconds = Math.floor(time % 60);
-      const milliseconds = Math.floor((time % 1) * 1000);
-      return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}:${milliseconds < 100 ? '0' : ''}${milliseconds < 10 ? '0' : ''}${milliseconds < 100 ? '0' : ''}${milliseconds}`;
-    };
-  
-    return (
-      <div className={styles.countdown}>
-        <span className={styles.timer}>{formatTime(remainingTime)}</span>
-      </div>
-    );
+  const targetTimeMillis = 1 * 60 * 1000; // 5 minutes in milliseconds
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (elapsedTime < targetTimeMillis) {
+        setElapsedTime(prevTime => prevTime + 10); // 10 milliseconds 간격으로 증가
+      } else {
+        setElapsedTime(0);
+      }
+    }, 10); // 10밀리초 간격으로 실행
+
+    return () => clearInterval(interval);
+  }, [elapsedTime]);
+
+  const remainingTimeMillis = targetTimeMillis - elapsedTime;
+
+  const formatTime = time => {
+    const minutes = Math.floor(time / (60 * 1000));
+    const seconds = Math.floor((time % (60 * 1000)) / 1000);
+    const milliseconds = time % 1000;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}:${milliseconds < 100 ? '0' : ''}${milliseconds < 10 ? '0' : ''}${milliseconds}`;
+  };
+
+  return (
+    <div className={styles.countdown}>
+      <span className={styles.timer}>{formatTime(remainingTimeMillis)}</span>
+    </div>
+  );
 };
 
 export default CountdownTimer;
