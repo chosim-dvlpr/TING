f57f984e859a390ae40f9db44a586edc4168f66b
이 은 석 <modelism93@naver.com>
2023-08-12 22:38:02 +0900

    feat: 결과 확인 클릭했을 때 결과 카드 확인

--- ting-frontend/src/component/matching/common/MatchingChoice.js
+++ ting-frontend/src/component/matching/common/MatchingChoice.js
@@ -4,60 +4,61 @@ import { useSelector } from "react-redux/es/hooks/useSelector"
 import { useNavigate } from "react-router-dom"
 import tokenHttp from "../../../api/tokenHttp.js"
 
-function MatchingChoice({session, count}){
+function MatchingChoice({ session, count }) {
   const navigate = useNavigate()
+  const [isFlipped, setIsFlipped] = useState(false)
 
-  const state = useSelector((state)=>state)
+  const state = useSelector((state) => state)
   const yourData = state.matchingReducer.yourData
   const userData = state.userdataReducer.userdata
   const matchingId = state.matchingReducer.matchingId
   const matchingResult = state.matchingReducer.matchingResult
-  
-  const [finish, setFinish] = useState(false) 
-  const [result,setResult] = useState(false)
+
+  const [finish, setFinish] = useState(false)
+  const [result, setResult] = useState(false)
   const [matchingFinish, setMatchingFinish] = useState(false)
-  const [buttonToggleSign, setButtonToggleSign] = useState([false,false])
+  const [buttonToggleSign, setButtonToggleSign] = useState([false, false])
 
   // 최종 선택 타이머 끝내는 로직
-  useEffect(()=>{
-    if (count === 0 && !matchingFinish){
+  useEffect(() => {
+    if (count === 0 && !matchingFinish) {
       setFinish(true)
       setMatchingFinish(true)
       becomeFriend()
     }
-  },[count]);
+  }, [count]);
 
   // 결과 선택 로직
-  useEffect(()=>{
+  useEffect(() => {
     sendResult(result)
     // 버튼 토글
-    if (result){
-      setButtonToggleSign([true,false])
+    if (result) {
+      setButtonToggleSign([true, false])
     }
     else {
-      setButtonToggleSign([false,true])
+      setButtonToggleSign([false, true])
     }
 
-  },[result])
+  }, [result])
 
   // 최종 결과 비동기로 친구 매칭 보내는 함수
-  const becomeFriend = async ()=>{
+  const becomeFriend = async () => {
     let resultData;
     if (result) {
-      resultData={
+      resultData = {
         matchingId: matchingId,
-        choice : 'yes'
+        choice: 'yes'
       };
     } else {
-      resultData={
+      resultData = {
         matchingId: matchingId,
-        choice : 'no'
+        choice: 'no'
       };
     }
     try {
-      const response = await tokenHttp.post('/date/result',resultData)
+      const response = await tokenHttp.post('/date/result', resultData)
       console.log(response.data);
-    } catch(err) {
+    } catch (err) {
       console.log(err)
     }
   }
@@ -65,59 +66,66 @@ function MatchingChoice({session, count}){
   // 상대에게 최종 결과를 전송하는 로직
   const sendResult = (result) => {
     session.signal({
-      data:JSON.stringify({ result : result, userId : userData.userId }),
-      to:[],
-      type:"select",
+      data: JSON.stringify({ result: result, userId: userData.userId }),
+      to: [],
+      type: "select",
     })
   }
 
-  return(
+  // 클릭시 뒤집어져서 결과 확인
+  const flipCard = (event) => {
+    setIsFlipped(!isFlipped)
+  }
+
+
+  return (
     <>
       <div className={styles.OuterContainer}>
-        <h1>최종 선택</h1>  
+        <h1>최종 선택</h1>
       </div>
-    
+
       <div className={styles.Container}>
 
         {/* 자신의 카드 */}
-        <div className={styles.flip}>  
-          <div className={`${styles.card}`} >
+        <div className={styles.flip}>
+          <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} >
             {/* 앞면 */}
             <div className={styles.front}></div>
             {/* 뒷면 */}
             <div className={styles.back}>
-              <p className={styles.CardContent}>{ result ? 'YES':'NO' }</p>
+              <p className={styles.CardContent}>{result ? 'YES' : 'NO'}</p>
             </div>
           </div>
         </div>
 
         <div className={styles.InnerBox}>
-          
-          { finish ? (
+
+          {finish ? (
             <div>
               <h3>모두 YES일 경우</h3>
               <h3>자동으로 어항에 추가됩니다.</h3>
-              <button onClick={()=>{window.location.href = '/'}}>메인으로 돌아가기</button>
+              <button onClick={() => { flipCard() }}>결과 확인하기</button>
+              <button className={styles.lastResultButton} onClick={() => { window.location.href = '/' }}>메인으로 돌아가기</button>
             </div>
           ) : (
-          <div>
-            <h3>{yourData.nickname}님을 선택하시겠습니까?</h3>
-            <h3>{ count }</h3>
-            <button className={buttonToggleSign[0] ? styles.clickedButton : styles.button} onClick={()=>{setResult(true);}}>YES</button>
-            <button className={buttonToggleSign[1] ? styles.clickedButton : styles.button} onClick={()=>{setResult(false);}}>NO</button>
-          </div>)
+            <div>
+              <h3>{yourData.nickname}님을 선택하시겠습니까?</h3>
+              <h3>{count}</h3>
+              <button className={buttonToggleSign[0] ? styles.clickedButton : styles.button} onClick={() => { setResult(true); }}>YES</button>
+              <button className={buttonToggleSign[1] ? styles.clickedButton : styles.button} onClick={() => { setResult(false); }}>NO</button>
+            </div>)
           }
         </div>
-        
+
         {/* 상대의 카드 */}
         {finish ? (
-          <div className={styles.flip}>  
-            <div className={`${styles.card}`} >
+          <div className={styles.flip}>
+            <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} >
               {/* 앞면 */}
               <div className={styles.yourFront}></div>
               {/* 뒷면 */}
               <div className={styles.back}>
-                <p className={styles.CardContent}>{ matchingResult.result ? 'YES':'NO' }</p>
+                <p className={styles.CardContent}>{matchingResult.result ? 'YES' : 'NO'}</p>
               </div>
             </div>
           </div>
