0a4012a68c596fe560ef39a3eaa13ce29d762bec
bmj0053 <bmj05378@gmail.com>
2023-08-12 22:55:07 +0900

    fix: signup complete 예외처리 수정

--- ting-frontend/src/component/signup/SignupComplete.js
+++ ting-frontend/src/component/signup/SignupComplete.js
@@ -18,7 +18,7 @@ function SignupComplete() {
       <div className={styles.completeContainer}>
         <h3>회원가입이 완료되었습니다</h3>
         <p className={styles.completeParagraph}>
-          { location.state.name }님에 대해 더 알려주시면 <br /> 저희가 최선의 소개팅 상대를 찾아드려요.
+          { location.state && location.state.name }님에 대해 더 알려주시면 <br /> 저희가 최선의 소개팅 상대를 찾아드려요.
         </p>
         <div className={styles.selectContainer}>
           <button className={styles.input} onClick={() => navigate("/signup/select")}>선택정보입력하기</button>
