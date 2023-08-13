632c2e5aec5ff488c93f188c795689c3ab8aa001
이 은 석 <modelism93@naver.com>
2023-08-14 01:06:22 +0900

    refactor: 물고기 랜덤 박스 모달 분리

--- ting-frontend/src/component/item/common/ItemUseModal.js
+++ ting-frontend/src/component/item/common/ItemUseModal.js
@@ -2,6 +2,8 @@ import { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import styles from "./ItemUseModal.module.css";
 
+import RandomFishModal from "./RandomFishModal";
+
 import tokenHttp from "../../../api/tokenHttp";
 
 function ItemModal({ closeModal, clickedItem }) {
@@ -67,38 +69,8 @@ function ItemModal({ closeModal, clickedItem }) {
     </div>
   );
 
-  function RandomFishModal({ randomFishData, closeModal }) {
 
-    const debrisCoordinates = [
-      { dx: -0.5, dy: -0.5, scale: 1 },
-      { dx: 0.5, dy: 0.5, scale:1 },
-      { dx: 0.2, dy: -0.6, scale:1 },
-      { dx: -0.8, dy: 0.2, scale:1 },
-      { dx: -0.3, dy: 0.6, scale:1 },
-      { dx: 0.6, dy: -0.3, scale:1 },
-      { dx: 0.7, dy: 0, scale:1 },
-      { dx: -0.7, dy: 0, scale:1 },
-      { dx: 0, dy: 0.7, scale:1 },
-      { dx: 0, dy: -0.7, scale:1 },
-    ];
-
-    return (
-      <>
-        <div className={styles.RandomFishModalInner}>
-          <img src={`${process.env.PUBLIC_URL}/img/closeIcon.png`} className={styles.closeButton} onClick={() => { closeModal() }} />
-          <h1>당신의 새로운 물고기</h1>
-          <img src={`https://i9b107.p.ssafy.io:5157/${randomFishData}`} className={styles.fishImg} />
-          {debrisCoordinates.map((coords, index) => (
-            <div
-              key={index}
-              className={styles.fishDebris}
-              style={{ "--dx": coords.dx, "--dy": coords.dy, "--scale": coords.scale, animationDelay: "0.5s" }}
-            ></div>
-          ))}
-        </div>
-      </>
-    )
-  }
+  
 
 }
 
