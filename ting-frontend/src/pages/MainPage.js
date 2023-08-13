0be56497a21cd796ed191c15f3217684fc3a4f95
정수정 <worend365@naver.com>
2023-08-14 01:04:14 +0900

    fix: 메인페이지 스크롤 막기

--- ting-frontend/src/pages/MainPage.js
+++ ting-frontend/src/pages/MainPage.js
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { useRef } from "react";
 // import { useNavigate } from "react-router-dom";
 
 import AOS from "aos";
 import "./aos.css";
 
 import styles from "./MainPage.module.css";
-import { SectionsContainer, Section } from "react-fullpage";
 
 //component
 import CountdownTimer from "../component/main/CountdownTimer";
@@ -17,6 +15,8 @@ import NavBar from "../component/common/NavBar";
 import FriendButton from "../component/common/FriendButton";
 import useMessageStore from "../component/friend/useMessageStore";
 import { useSelector } from "react-redux";
+
+import MyContext from "../MyContext"; // 생성한 Context 가져오기
 
 function MainPage() {
   // 채팅 기능 추가 //
@@ -38,105 +38,117 @@ function MainPage() {
   }, [userdata]);
   // 여기까지 채팅 //
 
-  useEffect(() => {
-    AOS.init();
-    return () => {
-      AOS.refresh(); // 컴포넌트가 언마운트될 때 AOS를 해제
-    };
-  }, []);
+  // useEffect(() => {
+  //   AOS.init();
+  //   return () => {
+  //     AOS.refresh(); // 컴포넌트가 언마운트될 때 AOS를 해제
+  //   };
+  // }, []);
 
   const outerDivRef = useRef();
-  useEffect(() => {
-    const wheelHandler = (e) => {
-      e.preventDefault();
-      const { deltaY } = e;
-      const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
-      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+  const wheelHandler = (e) => {
+    // e.preventDefault();
+    const { deltaY } = e;
+    const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+    const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
 
-      if (deltaY > 0) {
-        // 스크롤 내릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight - 5) {
-          //현재 1페이지
-          console.log(scrollTop, pageHeight);
-          outerDivRef.current.scrollTo({
-            top: pageHeight,
-            left: 0,
-            behavior: "smooth",
-          });
-        } else if (
-          scrollTop >= pageHeight - 5 &&
-          scrollTop < pageHeight * 2 - 5
-        ) {
-          //현재 2페이지
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 2,
-            left: 0,
-            behavior: "smooth",
-          });
-        } else if (
-          scrollTop >= pageHeight * 2 - 5 &&
-          scrollTop < pageHeight * 3 - 5
-        ) {
-          // 현재 3페이지
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 3,
-            left: 0,
-            behavior: "smooth",
-          });
-        } else {
-          // 현재 4페이지
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 4,
-            left: 0,
-            behavior: "smooth",
-          });
-        }
+    if (deltaY > 0) {
+      // 스크롤 내릴 때
+      if (scrollTop >= 0 && scrollTop < pageHeight - 5) {
+        //현재 1페이지
+        outerDivRef.current.scrollTo({
+          top: pageHeight,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (
+        scrollTop >= pageHeight - 5 &&
+        scrollTop < pageHeight * 2 - 5
+      ) {
+        //현재 2페이지
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 2,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (
+        scrollTop >= pageHeight * 2 - 5 &&
+        scrollTop < pageHeight * 3 - 5
+      ) {
+        // 현재 3페이지
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 3,
+          left: 0,
+          behavior: "smooth",
+        });
       } else {
-        // 스크롤 올릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight + 5) {
-          //현재 1페이지
-          outerDivRef.current.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          });
-        } else if (
-          scrollTop >= pageHeight + 5 &&
-          scrollTop < pageHeight * 2 + 5
-        ) {
-          //현재 2페이지
-          outerDivRef.current.scrollTo({
-            top: pageHeight,
-            left: 0,
-            behavior: "smooth",
-          });
-        } else if (
-          scrollTop >= pageHeight * 2 + 5 &&
-          scrollTop < pageHeight * 3 + 5
-        ) {
-          // 현재 3페이지
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 2,
-            left: 0,
-            behavior: "smooth",
-          });
-        } else {
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 3,
-            left: 0,
-            behavior: "smooth",
-          });
-        }
+        // 현재 4페이지
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 4,
+          left: 0,
+          behavior: "smooth",
+        });
       }
-    };
-    const outerDivRefCurrent = outerDivRef.current;
-    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+    } else {
+      // 스크롤 올릴 때
+      if (scrollTop >= 0 && scrollTop < pageHeight + 5) {
+        //현재 1페이지
+        outerDivRef.current.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (
+        scrollTop >= pageHeight + 5 &&
+        scrollTop < pageHeight * 2 + 5
+      ) {
+        //현재 2페이지
+        outerDivRef.current.scrollTo({
+          top: pageHeight,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (
+        scrollTop >= pageHeight * 2 + 5 &&
+        scrollTop < pageHeight * 3 + 5
+      ) {
+        // 현재 3페이지
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 2,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else {
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 3,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const scrollStyle = () => {
+    outerDivRef.current.className = styles.outer;
+  };
+
+  const noScrollStyle = () => {
+    outerDivRef.current.className = styles.outerNoScroll;
+  };
+
+  useEffect(() => {
+    window.addEventListener("wheel", wheelHandler, { once: true });
+    window.addEventListener("DOMMouseScroll", wheelHandler, { once: true });
+    window.addEventListener("mousewheel", wheelHandler, { once: true });
+
     return () => {
-      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+      window.removeEventListener("wheel", wheelHandler);
+      window.removeEventListener("DOMMouseScroll", wheelHandler);
+      window.removeEventListener("mousewheel", wheelHandler);
     };
   }, []);
 
-  let optinos = {
+  let options = {
     anchors: [
       "sectionOne",
       "sectionTwo",
@@ -147,165 +159,164 @@ function MainPage() {
   };
 
   return (
-    <div ref={outerDivRef} className={styles.outer}>
-      <NavBar />
-      {
-        userdata &&
-        <FriendButton />
-      }
+    <MyContext.Provider value={{ wheelHandler, scrollStyle, noScrollStyle }}>
+      <div ref={outerDivRef} className={styles.outer}>
+        <NavBar />
+        {userdata && <FriendButton />}
 
-      {/*section1 */}
-      <div className={styles.wrapper}>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        <div class={styles.snowflake}>
-          <img src="/img/heart_1.png"></img>
-        </div>
-        {/* <div className={styles.backgroundFish} id={styles.backgroundFish1}>
+        {/*section1 */}
+        <div className={styles.wrapper}>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          <div class={styles.snowflake}>
+            <img src="/img/heart_1.png"></img>
+          </div>
+          {/* <div className={styles.backgroundFish} id={styles.backgroundFish1}>
           <img src="/img/fish_group_1.png"></img>
         </div>
         <div className={styles.backgroundFish} id={styles.backgroundFish2}>
           <img src="/img/fish_group_2.png"></img>
         </div> */}
-        <div className={styles.section1textbox}>
-          <h1>
-            5분 <span id={styles.textDeco}> TING!! </span>으로<br></br>사랑을
-            찾아봐
-          </h1>
-        </div>
-        <div className={styles.section1imgbox}>
-          {/* <Link to="/matching" className={styles.linkButton}>
+          <div className={styles.section1textbox}>
+            <h1>
+              5분 <span id={styles.textDeco}> TING!! </span>으로<br></br>사랑을
+              찾아봐
+            </h1>
+          </div>
+          <div className={styles.section1imgbox}>
+            {/* <Link to="/matching" className={styles.linkButton}>
             <img src="/img/heart_1.png" alt="main" className={styles.img}></img>
           </Link> */}
-          <MainButton></MainButton>
-        </div>
-        <div className={styles.down}>
-          <span>팅은 어떤 서비스인가요?</span>↓
-        </div>
-        {/* <div>
+            <MainButton></MainButton>
+          </div>
+          <div className={styles.down}>
+            <span>팅은 어떤 서비스인가요?</span>↓
+          </div>
+          {/* <div>
           <img src="/img/arrow.png"></img>
         </div> */}
-      </div>
+        </div>
 
-      {/*section2 */}
-      <div
-        // data-aos="fade-down"
-        // data-aos-duration="1000"
-        className={styles.wrapper}
-      >
-        <img
-          src="/img/watch.png"
-          alt="watch"
-          className={styles.section2imgbox}
-        ></img>
+        {/*section2 */}
+        <div
+          // data-aos="fade-down"
+          // data-aos-duration="1000"
+          className={styles.wrapper}
+        >
+          <img
+            src="/img/watch.png"
+            alt="watch"
+            className={styles.section2imgbox}
+          ></img>
 
-        <div className={styles.section2textbox}>
-          <CountdownTimer />
-          <h2>
-            5분 동안 어떤 일을<br></br>할 수 있을까요?
-          </h2>
+          <div className={styles.section2textbox}>
+            <CountdownTimer />
+            <h2>
+              5분 동안 어떤 일을<br></br>할 수 있을까요?
+            </h2>
+          </div>
+        </div>
+
+        {/*section3 */}
+        <div
+          // data-aos="fade-down"
+          // data-aos-duration="1000"
+          className={styles.wrapper}
+        >
+          <img
+            src="/img/cards_front.png"
+            alt="cards_front"
+            className={styles.section3imgbox1}
+          ></img>
+          <img
+            src="/img/cards_left.png"
+            alt="cards_left"
+            className={styles.section3imgbox2}
+          ></img>
+          <div className={styles.section2textbox}>
+            <h2>
+              팅에서는
+              <br />
+              5분 안에
+              <br />
+              상대의 취미부터 취향까지
+              <br />
+              바로 확인 할 수 있습니다
+            </h2>
+          </div>
+        </div>
+
+        {/*section4 */}
+        <div
+          // data-aos="fade-down"
+          // data-aos-duration="1000"
+          className={styles.wrapper}
+        >
+          <div className={styles.section2textbox}>
+            <h2>
+              5분 후<br />
+              상대에게
+              <br />
+              마음을 표현해보세요
+            </h2>
+          </div>
+          <img
+            src="/img/hearthand.png"
+            alt="hearthand"
+            className={styles.section4imgbox}
+          ></img>
+        </div>
+
+        {/* section5 */}
+        <div
+          // data-aos="fade-down"
+          // data-aos-duration="1000"
+          className={styles.wrapper}
+        >
+          <div className={styles.backgroundFish} id={styles.backgroundFish1}>
+            <img src="/img/fish_group_1.png"></img>
+          </div>
+          <div className={styles.backgroundFish} id={styles.backgroundFish2}>
+            <img src="/img/fish_group_2.png"></img>
+          </div>
+          <img
+            src="/img/click.png"
+            alt="click"
+            className={styles.section5imgbox}
+          ></img>
+
+          <div className={styles.section5textbox}>
+            <h1>지금 바로</h1>
+            <MainButton />
+          </div>
         </div>
       </div>
-
-      {/*section3 */}
-      <div
-        // data-aos="fade-down"
-        // data-aos-duration="1000"
-        className={styles.wrapper}
-      >
-        <img
-          src="/img/cards_front.png"
-          alt="cards_front"
-          className={styles.section3imgbox1}
-        ></img>
-        <img
-          src="/img/cards_left.png"
-          alt="cards_left"
-          className={styles.section3imgbox2}
-        ></img>
-        <div className={styles.section2textbox}>
-          <h2>
-            팅에서는
-            <br />
-            5분 안에
-            <br />
-            상대의 취미부터 취향까지
-            <br />
-            바로 확인 할 수 있습니다
-          </h2>
-        </div>
-      </div>
-
-      {/*section4 */}
-
-      <div
-        // data-aos="fade-down"
-        // data-aos-duration="1000"
-        className={styles.wrapper}
-      >
-        <div className={styles.section2textbox}>
-          <h2>
-            5분 후<br />
-            상대에게
-            <br />
-            마음을 표현해보세요
-          </h2>
-        </div>
-        <img
-          src="/img/hearthand.png"
-          alt="hearthand"
-          className={styles.section4imgbox}
-        ></img>
-      </div>
-
-      <div
-        // data-aos="fade-down"
-        // data-aos-duration="1000"
-        className={styles.wrapper}
-      >
-        <div className={styles.backgroundFish} id={styles.backgroundFish1}>
-          <img src="/img/fish_group_1.png"></img>
-        </div>
-        <div className={styles.backgroundFish} id={styles.backgroundFish2}>
-          <img src="/img/fish_group_2.png"></img>
-        </div>
-        <img
-          src="/img/click.png"
-          alt="click"
-          className={styles.section5imgbox}
-        ></img>
-
-        <div className={styles.section5textbox}>
-          <h1>지금 바로</h1>
-          <MainButton />
-        </div>
-      </div>
-    </div>
+    </MyContext.Provider>
   );
 }
 
