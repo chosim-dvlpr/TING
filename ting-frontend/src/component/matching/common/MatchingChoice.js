9e28b94e1d64b8c7bd1c38247f107062776d0463
이 은 석 <modelism93@naver.com>
2023-08-14 13:28:54 +0900

    feat: 최종 결과 선택 카드 뒤집기 설정 변경

--- ting-frontend/src/component/matching/common/MatchingChoice.js
+++ ting-frontend/src/component/matching/common/MatchingChoice.js
@@ -3,6 +3,7 @@ import styles from "./MatchingChoice.module.css"
 import { useSelector } from "react-redux/es/hooks/useSelector"
 import { useNavigate } from "react-router-dom"
 import tokenHttp from "../../../api/tokenHttp.js"
+import MatchingResult from "./MatchingResult"
 
 function MatchingChoice({ session, count }) {
   const navigate = useNavigate()
@@ -18,6 +19,7 @@ function MatchingChoice({ session, count }) {
   const [result, setResult] = useState(false)
   const [matchingFinish, setMatchingFinish] = useState(false)
   const [buttonToggleSign, setButtonToggleSign] = useState([false, false])
+  const [afterFirstClick, setAfterFirstClick] = useState(false)
 
   // 최종 선택 타이머 끝내는 로직
   useEffect(() => {
@@ -65,11 +67,13 @@ function MatchingChoice({ session, count }) {
 
   // 상대에게 최종 결과를 전송하는 로직
   const sendResult = (result) => {
-    session.signal({
-      data: JSON.stringify({ result: result, userId: userData.userId }),
-      to: [],
-      type: "select",
-    })
+    if (session) {
+      session.signal({
+        data: JSON.stringify({ result: result, userId: userData.userId }),
+        to: [],
+        type: "select",
+      })
+    }
   }
 
   // 클릭시 뒤집어져서 결과 확인
@@ -80,6 +84,9 @@ function MatchingChoice({ session, count }) {
 
   return (
     <>
+      {/* 최종 결과 */}
+      {finish ? <MatchingResult /> : null}
+
       <div className={styles.OuterContainer}>
         <h1>최종 선택</h1>
       </div>
@@ -90,7 +97,15 @@ function MatchingChoice({ session, count }) {
         <div className={styles.flip}>
           <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} >
             {/* 앞면 */}
-            <div className={styles.front}></div>
+            {afterFirstClick ? (
+              <div className={styles.front2}>
+                <p className={styles.CardContent}>{result ? 'YES' : 'NO'}</p>
+              </div>
+            ) : (
+              // 첫 클릭일 때
+              <div className={styles.front}></div>
+            )}
+
             {/* 뒷면 */}
             <div className={styles.back}>
               <p className={styles.CardContent}>{result ? 'YES' : 'NO'}</p>
@@ -99,38 +114,27 @@ function MatchingChoice({ session, count }) {
         </div>
 
         <div className={styles.InnerBox}>
-
-          {finish ? (
-            <div>
-              <h3>모두 YES일 경우</h3>
-              <h3>자동으로 어항에 추가됩니다.</h3>
-              <button onClick={() => { flipCard() }}>결과 확인하기</button>
-              <button className={styles.lastResultButton} onClick={() => { window.location.href = '/' }}>메인으로 돌아가기</button>
-            </div>
-          ) : (
-            <div>
-              <h3>{yourData.nickname}님을 선택하시겠습니까?</h3>
-              <h3>{count}</h3>
-              <button className={buttonToggleSign[0] ? styles.clickedButton : styles.button} onClick={() => { setResult(true); }}>YES</button>
-              <button className={buttonToggleSign[1] ? styles.clickedButton : styles.button} onClick={() => { setResult(false); }}>NO</button>
-            </div>)
-          }
-        </div>
-
-        {/* 상대의 카드 */}
-        {finish ? (
-          <div className={styles.flip}>
-            <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} >
-              {/* 앞면 */}
-              <div className={styles.yourFront}></div>
-              {/* 뒷면 */}
-              <div className={styles.back}>
-                <p className={styles.CardContent}>{matchingResult.result ? 'YES' : 'NO'}</p>
-              </div>
-            </div>
+          <div>
+            <h3>{yourData.nickname}님을 선택하시겠습니까?</h3>
+            <h3>{count}</h3>
+            <button
+              className={`styles.Btn ${buttonToggleSign[0] ? styles.clickedButton : styles.button}`}
+              onClick={() => {
+                setResult(true);
+                flipCard();
+                setAfterFirstClick(true);
+              }}
+            >YES</button>
+            <button
+              className={`styles.Btn ${buttonToggleSign[1] ? styles.clickedButton : styles.button}`}
+              onClick={() => {
+                setResult(false);
+                flipCard();
+                setAfterFirstClick(true);
+              }}
+            >NO</button>
           </div>
-        ) : (null)}
-
+        </div>
       </div>
     </>
   )
