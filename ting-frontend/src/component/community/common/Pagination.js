17ec2dc835e0b795cc09c0f80b71840b5d3e0f06
정수정 <worend365@naver.com>
2023-08-16 04:17:31 +0900

    fix: 페이지네이션 수정

--- ting-frontend/src/component/community/common/Pagination.js
+++ ting-frontend/src/component/community/common/Pagination.js
@@ -3,51 +3,77 @@ import { useEffect, useState } from "react";
 import styles from "./Pagination.module.css";
 
 function Pagination({ currentPage, totalPages, onPageChange }) {
-    const [currentClick, setCurrentClick] = useState(0);
-    const [prevClick, setPrevClick] = useState(null);
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [currentClick, setCurrentClick] = useState(0);
+  const [prevClick, setPrevClick] = useState(null);
+  const [tenth, setTenth] = useState();
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    useEffect((e) => {
-        if(currentClick != null) {
-          let current = document.getElementById(currentClick);
-          current.style.backgroundColor = "#d75c6b";
-          // current.style.borderBottom = "2px solid black";
-        }
-        if(prevClick != null) {
-          let prev = document.getElementById(prevClick);
-          prev.style.backgroundColor = "#e58490";
-          // prev.style.borderBottom = "none";
-        }
-        setPrevClick(currentClick);
-      },[currentClick])
+  useEffect(() => {
+    console.log(currentPage);
+    setTenth(Math.floor((currentPage - 1) / 10) * 10);
+    console.log("tenth", tenth);
+  }, [currentPage]);
 
-    const getClick = (e) => {
-        console.log(e.target.id);
+  const getClick = (e) => {
+    console.log(e.target.id);
     setCurrentClick(e.target.id);
-    }
+  };
 
-    const check = (currentPage, index, totalPages) => {
-        if(((Math.floor(currentPage/10))*10 + index + 1) > totalPages) return false;
-        return true;
-    }
+  const check = (index, totalPages) => {
+    if (tenth + index + 1 > totalPages) return false;
+    return true;
+  };
 
-    return (
-        <div className={styles.pagination}>
-            {Math.floor(currentPage/10)*10 > 0? <button>{"<<"}</button>: ""}
-            {arr.map((num, index) => (
-                    check(currentPage, index, totalPages) &&
-                    (<button
-                        id={index}
-                        key={index + 1}
-                        className={currentPage === index + 1 ? styles.activePage : styles.page}
-                        onClick={(e) => {onPageChange(index + 1); getClick(e);}}
-                    >
-                        {(Math.floor(currentPage/10))*10 + index + 1}
-                    </button>)
-            ))}
-            {(Math.floor(currentPage/10)+1)*10 < totalPages? <button>{">>"}</button>: ""}
-        </div>
-    );
+  return (
+    <div className={styles.pagination}>
+      {tenth > 0 ? (
+        <button
+          className={styles.btn}
+          id="prevBtn"
+          onClick={(e) => {
+            onPageChange(tenth - 9);
+            getClick(e);
+          }}
+        >
+          {"<<"}
+        </button>
+      ) : (
+        ""
+      )}
+      {arr.map(
+        (num, index) =>
+          check(index, totalPages) && (
+            <button
+              id={index}
+              key={index + 1}
+              className={
+                currentPage === tenth + num ? styles.activePage : styles.page
+              }
+              onClick={(e) => {
+                onPageChange(tenth + num);
+                getClick(e);
+              }}
+            >
+              {tenth + num}
+            </button>
+          )
+      )}
+      {tenth + 11 < totalPages ? (
+        <button
+          className={styles.btn}
+          id="nextBtn"
+          onClick={(e) => {
+            onPageChange(tenth + 11);
+            getClick(e);
+          }}
+        >
+          {">>"}
+        </button>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default Pagination;
