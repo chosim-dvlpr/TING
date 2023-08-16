c5335f0bc2390934dd0f0e2763dd49a089b60159
정수정 <worend365@naver.com>
2023-08-16 13:15:36 +0900

    fix: 네비바 팅 버튼 추가 및 튜토리얼 수정

--- ting-frontend/src/pages/TutorialPage.js
+++ ting-frontend/src/pages/TutorialPage.js
@@ -67,7 +67,12 @@ function TutorialPage() {
         </div>
 
         {/* section3 */}
-        <div className={styles.bigImgSection}>
+        <div className={styles.section}>
+          <img
+            className={styles.img}
+            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_2.PNG"}
+            alt="main"
+          />
           <div className={styles.description}>
             <div className={styles.numberImgPink}>3</div>
             <div>
@@ -80,32 +85,32 @@ function TutorialPage() {
               당신의 인연을 찾아드릴게요!
             </div>
           </div>
-          <img
-            className={styles.img}
-            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_2.PNG"}
-            alt="main"
-          />
         </div>
 
         {/* section4 */}
         <div className={styles.section}>
-          <img
-            className={styles.img}
-            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_3.PNG"}
-            alt="main"
-          />
           <div className={styles.description}>
             <div className={styles.numberImgBlue}>4</div>
             <div>
               매칭 알림을 받으면
               <br />
-              15초 안에 수락 버튼을 눌러주세요
+              15초 안에 수락 버튼을 눌러주세요.
             </div>
           </div>
+          <img
+            className={styles.img}
+            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_3.PNG"}
+            alt="main"
+          />
         </div>
 
         {/* section5 */}
         <div className={styles.section}>
+          <img
+            className={styles.img}
+            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_5.png"}
+            alt="main"
+          />
           <div className={styles.description}>
             <div className={styles.numberImgPink}>5</div>
             <div>
@@ -113,15 +118,10 @@ function TutorialPage() {
               매칭 상대를 알아가 보세요!
             </div>
           </div>
-          <img
-            className={styles.img}
-            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_5.PNG"}
-            alt="main"
-          />
         </div>
 
         {/* section6 */}
-        <div className={styles.bigImgSection}>
+        <div className={styles.section}>
           <div className={styles.description}>
             <div className={styles.numberImgBlue}>6</div>
             <div>
@@ -129,12 +129,45 @@ function TutorialPage() {
               <br />
               당신의 마지막 선택으로
               <br />
-              친구 추가 여부가 결정됩니다
+              친구 추가 여부가 결정됩니다.
             </div>
           </div>
           <img
             className={styles.img}
             src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_9.PNG"}
+            alt="main"
+          />
+        </div>
+
+        {/* section7 */}
+        <div className={styles.section}>
+          <img
+            className={styles.img}
+            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_12.png"}
+            alt="main"
+          />
+          <div className={styles.description}>
+            <div className={styles.numberImgPink}>7</div>
+            <div>
+              서로를 선택한 경우 <br />
+              어항에 친구 물고기가 추가되어 <br />
+              채팅을 할 수 있습니다.
+            </div>
+          </div>
+        </div>
+
+        {/* section8 */}
+        <div className={styles.section}>
+          <div className={styles.description}>
+            <div className={styles.numberImgBlue}>8</div>
+            <div>
+              채팅방에서는 상대방의 프로필과<br></br>
+              채팅방 온도를 확인할 수 있습니다!
+            </div>
+          </div>
+          <img
+            className={styles.img}
+            src={process.env.PUBLIC_URL + "/img/tutorial/tutorial_13.PNG"}
             alt="main"
           />
         </div>
