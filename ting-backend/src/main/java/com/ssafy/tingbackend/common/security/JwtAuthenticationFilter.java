49c33fb082768bcfee4416fd8e6fb904a82629b5
cksghks89 <song960705@naver.com>
2023-08-17 17:13:56 +0900

    feat: 관리자 계정 security 검증 추가

--- ting-backend/src/main/java/com/ssafy/tingbackend/common/security/JwtAuthenticationFilter.java
+++ ting-backend/src/main/java/com/ssafy/tingbackend/common/security/JwtAuthenticationFilter.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,6 +15,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,6 +32,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             UsernamePasswordAuthenticationToken authenticationToken
                     = new UsernamePasswordAuthenticationToken(userId, null, null);
+
+            if ("6".equals(userId)) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+                authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, authorities);
+                log.info("authenticationToken : {}", authenticationToken);
+            }
 
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
